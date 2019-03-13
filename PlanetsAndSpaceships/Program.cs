790e673fe4accaf91f73a8be3b36e6e16c991b4e
unknown <f.mulugeta90@gmail.com>
2019-03-13 10:55:56 -0500

    done with planet and spaceship

---
+++ PlanetsAndSpaceships/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetsAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+            List<string> LastTwoPlanets = new List<string> { "Uranus", "Neptune" };
+            planetList.AddRange(LastTwoPlanets);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+            planetList.Remove("Pluto");
+
+            List<string> spaceshipsVisitedMercury = new List<string>() { "Mariner", "MESSENGER", "BepiColombo" };
+            List<string> spaceshipsVistitedVenus = new List<string>() { "Mariner", "Venera 6", "Cosmos 96" };
+            List<string> spacesshipsVisitedMars = new List<string>() { "Viking", "Opportunity", "Curiosity" };
+            var spaceship = new Dictionary<string, List<string>>
+            {
+                { "Mercury", spaceshipsVisitedMercury },
+                { "Venus", spaceshipsVistitedVenus },
+                { "Mars", spacesshipsVisitedMars }
+            };
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+            
+            foreach (var planet in planetList)
+            {
+               // Console.WriteLine(planet);
+                foreach (var (planetName, spaceShipsVisitedPlanets) in spaceship)
+                {
+                    if (planet == planetName)
+                    {
+                        Console.Write($"{planet}: ");
+                        var space = "";
+                        //Console.WriteLine($"{planetName} :");
+                        foreach (var spaceShipVisitedPlanet in spaceShipsVisitedPlanets)
+                        {
+                            space += $"{spaceShipVisitedPlanet} ,";
+                           // Console.Write($"{spaceShipVisitedMercury}, ");
+                            Console.Write("");
+                        }
+                        Console.WriteLine(space);
+                    }
+                }
+               
+
+            }
+
+            Console.Write("Rockey planets:");
+
+            foreach (var rockyPlanet in rockyPlanets)
+            {
+               
+                Console.Write($"{rockyPlanet },");
+            }
+
+
+           // var hello = new planetsListSpaceships();
+           // hello.PlanetInfo();
+           Console.ReadLine();
+        }
+     
+    }
+}
---
+++ PlanetsAndSpaceships/planetsListSpaceships.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetsAndSpaceships
+{
+    class planetsListSpaceships
+    {
+        //List<string> planetList = new List<string>() { "Mercury", "Mars" };
+        //planetList.Add("Jupiter");
+        //    planetList.Add("Saturn");
+        //    List<string> LastTwoPlanets = new List<string> { "Uranus", "Neptune" };
+        //planetList.AddRange(LastTwoPlanets);
+
+        public void PlanetInfo()
+        {
+            
+
+
+            //foreach (var planet in planetList)
+            //{
+            //    Console.WriteLine(planetList);
+               
+            //}
+        }
+    }
+}
